34e12db65bb42f175cf61df5de054388b9c4dcca
Konstantin Urban <kontenurban@gmail.com>
2020-04-14 09:46:15 +0200

    added loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'urbanwheels';
+  loading = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = false;
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = true;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
 }
